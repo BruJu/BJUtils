d2a3d2b693841bdfcc7c1e84f87df688cfdbe030
Bruju <bruyat.ju@live.fr>
2019-12-20 05:26:49 +0100

    Ajout de ListUtils::filterMeilleurs (issu de https://github.com/BruJu/tp-ia-mdp/blob/master/src/utilitaire/UtilsMDP.java )

--- src/fr/bruju/util/ListUtils.java
+++ src/fr/bruju/util/ListUtils.java
@@ -124,4 +124,35 @@ public class ListUtils {
         return explores;
     }
 
+	/**
+	 * Filtre les éléments de la liste pour renvoyer une liste ne contenant que les meilleurs éléments
+	 * @param values Les éléments
+	 * @param evaluation La fonction d'évaluation d'un élément
+	 * @return La liste des éléments dont l'évaluation est la meilleure
+	 */
+	public static <T> Collection<T> filterBest(Collection<T> values, Function<T, Double> evaluation) {
+		List<T> best = new ArrayList<>();
+		
+		Double bestValue = null;
+		
+		for (T value : values) {
+			Double eval = evaluation.apply(value);
+			
+			if (eval == null)
+				continue;
+			
+			if (bestValue != null) {
+				if (eval < bestValue)
+					continue;
+				
+				if (eval > bestValue)
+					best.clear();
+			}
+			
+			best.add(value);
+			bestValue = eval;
+		}
+		
+		return best;
+	}
 }
